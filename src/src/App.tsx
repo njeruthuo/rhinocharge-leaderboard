da97f58237b6805af31697278e52b876ed207bfe
njeruthuo <juliusn411@gmail.com>
2026-04-10 12:58:50 +0300

    docs: changed core content

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,4 +1,6 @@
-import SafariLeaderboard from "./SafariLeaderboard";
+// import SafariLeaderboard from "./SafariLeaderboard";
+
+import SafariLeaderboard from "./SafariLeaderboardv2";
 
 export default function App() {
   return <SafariLeaderboard />;
