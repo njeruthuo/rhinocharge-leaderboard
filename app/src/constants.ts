fee49cdb330f90af1c401e3e71d0200a9858d764
njeruthuo <juliusn411@gmail.com>
2026-04-20 11:31:14 +0300

    fix: changed loading icon

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,4 +1,4 @@
-import spinner from "@/assets/progress_activity_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
+import spinner from "@/assets/progress_activity_24dp_434343_FILL0_wght400_GRAD0_opsz24.svg";
 
 export const BASE_URL = import.meta.env.VITE_APP_BASE;
 export { spinner };
